fa2a48e8ff4187de4b2f3a181f68d6fe1c8e2913
Jose Arce y de la Borbolla <joseab10@gmail.com>
2019-01-25 21:55:17 +0100

    Merge

--- src/cls/airsim/as_env.py
+++ src/cls/airsim/as_env.py
@@ -1,6 +1,8 @@
 import subprocess
+import csv
 
-from sys import platform, stdout
+
+from sys import platform, argv, stdout
 from os import path, remove
 from time import sleep
 from enum import Enum
@@ -22,20 +24,33 @@ class Environments(Enum):
 # CONSTANTS
 
 class DroneActIdx(Enum):
-    Pitch    = 0
-    Yaw      = 1
-    Roll     = 2
-    Throttle = 3
+    VX       = 0
+    VY       = 1
+    VZ       = 2
+    Yaw     = 3
+#    Pitch    = 0
+#    Yaw      = 1
+#    Roll     = 2
+#    Throttle = 3
+
 
 class DroneAct(Enum):
-    PitchFW = 0
-    PitchBW = 1
-    RollLF  = 2
-    RollRG  = 3
-    YawLF   = 4
-    YawRG   = 5
-    ThroUP  = 6
-    ThroDW  = 7
+    VxFW     = 0
+    VxBW     = 1
+    VyFW     = 2
+    VyBW     = 3
+    VzFW     = 4
+    VzBW     = 5
+    YawR    = 6
+    YawL    = 7
+#    PitchFW = 0
+#    PitchBW = 1
+#    RollLF  = 2
+#    RollRG  = 3
+#    YawLF   = 4
+#    YawRG   = 5
+#    ThroUP  = 6
+#    ThroDW  = 7
 
 class CarActIdx(Enum):
     VX       = 0
@@ -64,7 +79,8 @@ class AS_Environment:
                  stepped_simulation:bool=True, step_duration:float=0.1,
                  manual_mode:bool=False, joystick:Union[int,None]=0,
                  rendering:bool=True, lowres:bool=True,
-                 crash_terminate=True, max_steps = 1000, min_reward=-1000):
+                 crash_terminate=True, max_steps = 1000, min_reward=-1000, 
+                 fast_deceleration = True):
 
         self.settings = AS_Settings(settings_path)
 
@@ -171,6 +187,7 @@ class AS_Environment:
         self.timeout = 15
 
         self.max_action_value = 250
+        self.fast_deceleration = fast_deceleration
 
         if not self.drone:
             self.client = airsim.CarClient(timeout_value=self.timeout)
@@ -203,22 +220,37 @@ class AS_Environment:
             vehicle_type = 'SimpleFlight'
 
             self.prev_act = [0, 0, 0, 0]
-
-            # Action conversion settings
-            pitch_roll_factor = 5
-            yaw_factor = 10
-            throttle_factor = 15
-
+            
+            # action constants
+            v_factor = 0.5
+            yaw_factor = 5.0
+            
             self.possible_actions = {
-                    DroneAct.PitchFW: {'index': DroneActIdx.Pitch   , 'sign':  1, 'factor': pitch_roll_factor, 'str': 'Pitch Fwd'},
-                    DroneAct.PitchBW: {'index': DroneActIdx.Pitch   , 'sign': -1, 'factor': pitch_roll_factor, 'str': 'Pitch Bwd'},
-                    DroneAct.RollRG : {'index': DroneActIdx.Roll    , 'sign':  1, 'factor': pitch_roll_factor, 'str': 'Roll Rght'},
-                    DroneAct.RollLF : {'index': DroneActIdx.Roll    , 'sign': -1, 'factor': pitch_roll_factor, 'str': 'Roll Left'},
-                    DroneAct.YawRG  : {'index': DroneActIdx.Yaw     , 'sign':  1, 'factor': yaw_factor       , 'str': 'Yaw Right'},
-                    DroneAct.YawLF  : {'index': DroneActIdx.Yaw     , 'sign': -1, 'factor': yaw_factor       , 'str': 'Yaw  Left'},
-                    DroneAct.ThroUP : {'index': DroneActIdx.Throttle, 'sign':  1, 'factor': throttle_factor  , 'str': 'Thrt   Up'},
-                    DroneAct.ThroDW : {'index': DroneActIdx.Throttle, 'sign': -1, 'factor': throttle_factor  , 'str': 'Thrt  Dwn'},
+                DroneAct.VxFW: {'index': DroneActIdx.VX, 'sign':  1, 'factor': v_factor, 'str': 'Vx Fwd'},
+                DroneAct.VxBW: {'index': DroneActIdx.VX, 'sign': -1, 'factor': v_factor, 'str': 'Vx Bwd'},
+                DroneAct.VyFW: {'index': DroneActIdx.VY, 'sign':  1, 'factor': v_factor, 'str': 'Vy Fwd'},
+                DroneAct.VyBW: {'index': DroneActIdx.VY, 'sign': -1, 'factor': v_factor, 'str': 'Vy Bwd'},
+                DroneAct.VzFW: {'index': DroneActIdx.VZ, 'sign':  1, 'factor': v_factor, 'str': 'Vz Fwd'},
+                DroneAct.VzBW: {'index': DroneActIdx.VZ, 'sign': -1, 'factor': v_factor, 'str': 'Vz Bwd'},
+                DroneAct.YawR: {'index': DroneActIdx.Yaw, 'sign': 1, 'factor': yaw_factor, 'str': 'Yaw Right'},
+                DroneAct.YawL: {'index': DroneActIdx.Yaw, 'sign': -1, 'factor': yaw_factor, 'str': 'Yaw Left'}, 
             }
+            
+#            # Action conversion settings
+#            pitch_roll_factor = 5
+#            yaw_factor = 10
+#            throttle_factor = 15
+#
+#            self.possible_actions = {
+#                    DroneAct.PitchFW: {'index': DroneActIdx.Pitch   , 'sign':  1, 'factor': pitch_roll_factor, 'str': 'Pitch Fwd'},
+#                    DroneAct.PitchBW: {'index': DroneActIdx.Pitch   , 'sign': -1, 'factor': pitch_roll_factor, 'str': 'Pitch Bwd'},
+#                    DroneAct.RollRG : {'index': DroneActIdx.Roll    , 'sign':  1, 'factor': pitch_roll_factor, 'str': 'Roll Rght'},
+#                    DroneAct.RollLF : {'index': DroneActIdx.Roll    , 'sign': -1, 'factor': pitch_roll_factor, 'str': 'Roll Left'},
+#                    DroneAct.YawRG  : {'index': DroneActIdx.Yaw     , 'sign':  1, 'factor': yaw_factor       , 'str': 'Yaw Right'},
+#                    DroneAct.YawLF  : {'index': DroneActIdx.Yaw     , 'sign': -1, 'factor': yaw_factor       , 'str': 'Yaw  Left'},
+#                    DroneAct.ThroUP : {'index': DroneActIdx.Throttle, 'sign':  1, 'factor': throttle_factor  , 'str': 'Thrt   Up'},
+#                    DroneAct.ThroDW : {'index': DroneActIdx.Throttle, 'sign': -1, 'factor': throttle_factor  , 'str': 'Thrt  Dwn'},
+#            }
 
         vehicle_settings_path = 'Vehicles/' + self.vehicle_name + '/'
         self.settings.set(vehicle_settings_path + 'VehicleType', vehicle_type)
@@ -394,9 +426,18 @@ class AS_Environment:
             self.client.simPause(False)
 
         if self.drone:
-            self.client.moveByAngleThrottleAsync(action[DroneActIdx.Pitch.value], action[DroneActIdx.Roll.value],
-                                                 action[DroneActIdx.Throttle.value], action[DroneActIdx.Yaw.value],
-                                                 self.step_duration).join()
+            # transform velocity according to the drone's orientation.... there is probably a way to do this more elegantly
+            yaw = airsim.to_eularian_angles(self.client.simGetVehiclePose().orientation)[2]
+            
+            v_x = (action[DroneActIdx.VX.value]*np.cos(yaw) - action[DroneActIdx.VY.value]*np.sin(yaw))
+            v_y = (action[DroneActIdx.VX.value]*np.sin(yaw) + action[DroneActIdx.VY.value]*np.cos(yaw))
+            
+            self.client.moveByVelocityAsync(v_x, v_y, action[DroneActIdx.VZ.value],self.step_duration,  
+                                        yaw_mode=airsim.YawMode(is_rate=True,  yaw_or_rate = action[DroneActIdx.Yaw.value])).join()
+#            self.client.moveByAngleThrottleAsync(action[DroneActIdx.Pitch.value], action[DroneActIdx.Roll.value],
+#                                                 action[DroneActIdx.Throttle.value], action[DroneActIdx.Yaw.value],
+#                                                 self.step_duration).join()
+            
 
         else:
             self.client.moveByVelocity(action[CarActIdx.VX.value], action[CarActIdx.VY.value], action[CarActIdx.VZ.value],
@@ -428,7 +469,7 @@ class AS_Environment:
         return state
 
 
-    def _reward_function(self, state):
+    def _reward_function(self, state, check_collision=True):
         reward = 0
 
         if self.current_target_idx < len(self.target_positions):
@@ -496,23 +537,96 @@ class AS_Environment:
 
         action_id = self._parse_actid(action_id)
 
-        action = np.zeros(4)
+        action = self.prev_act #np.zeros(4)
         act = self.possible_actions[action_id]
 
-        tmp_act = self.prev_act[act['index'].value] + act['sign'] * act['factor']
-        if tmp_act < self.max_action_value:
-            action[act['index'].value] = tmp_act
-        else:
+        prev_val = self.prev_act[act['index'].value]
+        sign = act['sign']
+        factor = act['factor']
+        tmp_act = prev_val + sign * factor
+        # decelaration should be faster than acceleration, so the agent can brake efficiently
+        if self.drone and self.fast_deceleration:
+            if sign * prev_val < 0.0:
+                tmp_act = prev_val + sign * factor * 3.0
+                # stop the deceleration if it goes beyond 0 
+                if sign * tmp_act > 0.0:
+                    tmp_act = 0.0
+        
+        if tmp_act > self.max_action_value:
             action[act['index'].value] = self.max_action_value
+        elif tmp_act < -self.max_action_value:
+            action[act['index'].value] = -self.max_action_value
+        else:
+            action[act['index'].value] = tmp_act
 
         self.prev_act = action
 
         return action
 
+    def read_recordings(self, rec_folders):
+        '''
+        parses the data from given AirSim recording folders into lists of states, action ids and rewards
+        (not implemented for car)
+        '''
+        states = []
+        action_ids = []
+        rewards = []
+        for folder in rec_folders:
+            with open(path.join(folder,  'airsim_rec.txt')) as data_file:
+                data_dicts = list(csv.DictReader(data_file,  delimiter='\t'))
+                
+                q = airsim.Quaternionr(w_val=float(data_dicts[0]["Q_W"]), x_val = float(data_dicts[0]["Q_X"]),
+                                                        y_val= float(data_dicts[0]["Q_Y"]), z_val=float(data_dicts[0]["Q_Z"]))
+                next_yaw = airsim.to_eularian_angles(q)[2]
+                for i in range (0, len(data_dicts)-1):
+                   # TODO store images instead of their paths 
+                    states.append(path.join(folder,  data_dicts[i]["ImageFile"]))
+                    # action choice
+                    yaw = next_yaw
+                    q = airsim.Quaternionr(w_val=float(data_dicts[i+1]["Q_W"]), x_val = float(data_dicts[i+1]["Q_X"]),
+                                                            y_val= float(data_dicts[i+1]["Q_Y"]), z_val=float(data_dicts[i+1]["Q_Z"]))
+                    next_yaw = airsim.to_eularian_angles(q)[2]
+                    
+                    
+                    time_diff = float(data_dicts[i+1]["TimeStamp"]) - float(data_dicts[i]["TimeStamp"])
+                    x_diff = float(data_dicts[i+1]["POS_X"]) - float(data_dicts[i]["POS_X"])
+                    y_diff = float(data_dicts[i+1]["POS_Y"]) - float(data_dicts[i]["POS_Y"])
+                    z_diff = float(data_dicts[i+1]["POS_Z"]) - float(data_dicts[i]["POS_Z"])
+                    yaw_diff = next_yaw -yaw
+                    
+                    # transform the positional velocities to velocity with regard to the drones's axis
+                    # TODO check signs, not entirely sure there
+                    yaw_avg = yaw+ yaw_diff/2.0 #average angle
+                    v_x = (x_diff*np.cos(yaw_avg) + y_diff*np.sin(yaw_avg))/time_diff
+                    v_y = (-x_diff*np.sin(yaw_avg) + y_diff*np.cos(yaw_avg))/time_diff
+                    v_z = z_diff/time_diff
+                   
+                    v_yaw = yaw_diff/time_diff
+                    # angular velocity probably needs to be scaled for comparison (TODO find suitable factor)
+                    v_yaw_scaling = 10.0 
+                    
+                    v_max_idx = np.argmax([abs(v) for v in [v_x,  v_y,  v_z,  v_yaw*v_yaw_scaling]])
+                    if v_max_idx == 0:
+                        if v_x >= 0: action = DroneAct.VxFW.value
+                        else: action = DroneAct.VxBW.value
+                    elif v_max_idx == 1:
+                        if v_y >= 0: action = DroneAct.VyFW.value
+                        else: action = DroneAct.VyBW.value
+                    elif v_max_idx == 2:
+                        if v_z >= 0: action = DroneAct.VzFW.value
+                        else: action = DroneAct.VzBW.value
+                    elif v_max_idx == 3:
+                        if v_yaw >= 0: action = DroneAct.YawR.value
+                        else: action = DroneAct.YawL.value
+                    action_ids.append(action)
+                    
+                    # rewards
+                    pos = airsim.Vector3r(x_val = float(data_dicts[i+1]["POS_X"]),
+                                                        y_val = float(data_dicts[i+1]["POS_Y"]),
+                                                        z_val = float(data_dicts[i+1]["POS_Z"]))
 
-
-
-
+                    rewards.append(self._reward_function({"pose": airsim.Pose(position_val=pos,  orientation_val=q)},  check_collision=False))
+        return states,  action_ids,  rewards
 
 
 # Class Tests
@@ -539,17 +653,23 @@ if __name__ == '__main__':
 
     target_positions = [(0, 0, 0), (1, 1, -1)]
 
+
     env = AS_Environment(target_positions, env=env, env_path=env_path,
-                         stepped_simulation=True, step_duration=1)
+                         stepped_simulation=False, step_duration=1)
+    env.reset()
 
     max_episodes = 3
 
     start_pos = [(0.5, 0.5, -2), (5, 5, -2), (3, 3, -2)]
-
+    
+    # fixed action sequence
+    # a_seq = [5, 0, 0, 2, 2, 2, 3, 6, 6, 6, 6, 6, 6, 6, 7, 7 , 7,  7,  7]
+    a_seq = [2, 2, 2, 2, 2, 2, 2]
+    
     # Possible Actions
     a = [0, 1, 2, 3, 4, 5, 6, 7]
     # Action Probabilities
-    p = [9, 9, 9, 9, 9, 9, 50, 0.1]
+    p = [20, 9, 9, 9, 9, 9, 5, 5]
     p /= np.sum(p)
 
     for episode in range(max_episodes):
